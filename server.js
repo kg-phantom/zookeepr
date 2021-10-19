a4afd534eebfdaf90f637b05e18000ff0f054190
Kyrsten <ksurbayan@gmail.com>
2021-10-19 00:42:12 -0800

    populate animals and zookeepers

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -65,7 +66,7 @@ function validateAnimal(animal) {
     if(!animal.diet || typeof animal.diet !== 'string') {
         return false;
     }
-    if(!animal.personalityTraits || typeof animal.personalityTraits !== 'string') {
+    if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
         return false;
     }
     return true;
@@ -102,6 +103,22 @@ app.post('/api/animals', (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+ 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
